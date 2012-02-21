cc0d168150119a32f654d419bc11211f0f1acb3a
Lennart C. Karssen <lennart@karssen.org>
2012-02-21 23:58:11 +0000

    Fixed a few typos in the palinear/palogist/pacoxph --help text.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/ProbABEL@843 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/usage.h
+++ src/usage.h
@@ -9,10 +9,10 @@ void print_usage (char * program_name, int exit_code)
 	fprintf(stdout,"\t --nids    : [optional] number of people to analyse\n");
 	fprintf(stdout,"\t --chrom   : [optional] chromosome (to be passed to output)\n");
 	fprintf(stdout,"\t --out     : [optional] output file name (default is regression.out.txt)\n");
-	fprintf(stdout,"\t --skipd   : [optional] how many columns to skip in predictor\n\t              (dose/prob) file (default 2)\n");
+	fprintf(stdout,"\t --skipd   : [optional] how many columns to skip in the predictor\n\t              (dose/prob) file (default 2)\n");
 #if COXPH
 	fprintf(stdout,"\t --ntraits : [optional] how many traits are analysed (default 2)\n");
-#else 
+#else
 	fprintf(stdout,"\t --ntraits : [optional] how many traits are analysed (default 1)\n");
 #endif
 	fprintf(stdout,"\t --ngpreds : [optional] how many predictor columns per marker\n\t              (default 1 = MLDOSE; else use 2 for MLPROB)\n");
@@ -20,8 +20,8 @@ void print_usage (char * program_name, int exit_code)
 	fprintf(stdout,"\t --score   : use score test\n");
 	fprintf(stdout,"\t --no-head : do not report header line\n");
 	fprintf(stdout,"\t --allcov  : report estimates for all covariates (large outputs!)\n");
-	fprintf(stdout,"\t --interaction: Which covariate to use for interaction with SNP analysys (default is no ineraction, 0)\n");
-	fprintf(stdout,"\t --interaction_only: like previos but without covariate acting in interaction with SNP (default is no ineraction, 0)\n");
+	fprintf(stdout,"\t --interaction: Which covariate to use for interaction with SNP analysys (default is no interaction, 0)\n");
+	fprintf(stdout,"\t --interaction_only: like previous but without covariate acting in interaction with SNP (default is no interaction, 0)\n");
 	fprintf(stdout,"\t --mmscore : score test in samples of related individuals. File with inverse of variance-covariance matrix (for palinear) or inverse correlation (for palogist) as input parameter\n");
 	fprintf(stdout,"\t --robust  : report robust (aka sandwich, aka Hubert-White) standard errors\n");
 	fprintf(stdout,"\t --help    : print help\n");
@@ -32,4 +32,3 @@ void print_help (char * program_name, int exit_code)
 {
 	print_usage(program_name,exit_code);
 }
-
