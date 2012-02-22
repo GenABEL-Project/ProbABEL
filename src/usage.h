38271f2c8483634cc0c483b8aacc24c64af28e93
Lennart C. Karssen <lennart@karssen.org>
2012-02-22 00:22:14 +0000

    Removed another small typo from the palinear/palogist/pacoxph help text.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/ProbABEL@844 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/usage.h
+++ src/usage.h
@@ -20,7 +20,7 @@ void print_usage (char * program_name, int exit_code)
 	fprintf(stdout,"\t --score   : use score test\n");
 	fprintf(stdout,"\t --no-head : do not report header line\n");
 	fprintf(stdout,"\t --allcov  : report estimates for all covariates (large outputs!)\n");
-	fprintf(stdout,"\t --interaction: Which covariate to use for interaction with SNP analysys (default is no interaction, 0)\n");
+	fprintf(stdout,"\t --interaction: Which covariate to use for interaction with SNP analysis (default is no interaction, 0)\n");
 	fprintf(stdout,"\t --interaction_only: like previous but without covariate acting in interaction with SNP (default is no interaction, 0)\n");
 	fprintf(stdout,"\t --mmscore : score test in samples of related individuals. File with inverse of variance-covariance matrix (for palinear) or inverse correlation (for palogist) as input parameter\n");
 	fprintf(stdout,"\t --robust  : report robust (aka sandwich, aka Hubert-White) standard errors\n");
