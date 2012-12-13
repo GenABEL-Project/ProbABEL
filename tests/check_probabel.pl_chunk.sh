7c571978ee3dc11004ee4291f280a859e3159383
Lennart C. Karssen <lennart@karssen.org>
2012-12-13 17:10:39 +0000

    ProbABEL: changed tabs to spaces in the probabel check script. No functional changes.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/ProbABEL@1054 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/check_probabel.pl_chunk.sh
+++ tests/check_probabel.pl_chunk.sh
@@ -44,59 +44,59 @@ echo "TestCohortChunk,$base.info,$base.dose,$base.prob,$base.map" \
 prepare_input ()
 {
     if [ "$1" = "nochunk" ]; then
-	# ------------------ No chunks test -------------------
-	# Split the dose, prob and info files up into two chromosomes
-	# with some  chunks
-	awk '{print $1,$2,$3,$4}'    $dosefile > chr1.dose
-	awk '{print $1,$2,$5,$6,$7}' $dosefile > chr2.dose
+        # ------------------ No chunks test -------------------
+        # Split the dose, prob and info files up into two chromosomes
+        # with some  chunks
+        awk '{print $1,$2,$3,$4}'    $dosefile > chr1.dose
+        awk '{print $1,$2,$5,$6,$7}' $dosefile > chr2.dose
 
-	awk '{print $1,$2,$3,$4,$5,$6}'          $probfile > chr1.prob
-	awk '{print $1,$2,$7,$8,$9,$10,$11,$12}' $probfile > chr2.prob
+        awk '{print $1,$2,$3,$4,$5,$6}'          $probfile > chr1.prob
+        awk '{print $1,$2,$7,$8,$9,$10,$11,$12}' $probfile > chr2.prob
 
-	sed -n '1,3p' $infofile >  chr1.info
-	sed -n '1p'   $infofile >  chr2.info
-	sed -n '4,6p' $infofile >> chr2.info
+        sed -n '1,3p' $infofile >  chr1.info
+        sed -n '1p'   $infofile >  chr2.info
+        sed -n '4,6p' $infofile >> chr2.info
 
-	sed -n '1,3p' $mapfile > chr1.map
-	sed -n '1p'   $mapfile >  chr2.map
-	sed -n '4,6p' $mapfile >> chr2.map
+        sed -n '1,3p' $mapfile > chr1.map
+        sed -n '1p'   $mapfile >  chr2.map
+        sed -n '4,6p' $mapfile >> chr2.map
 
-	WithOrWithout="without"
+        WithOrWithout="without"
     elif [ "$1" = "chunk" ]; then
-	# ------------------ Chunks test ----------------------
-	# Split the dose and info files up into two chromosomes with
-	# some chunks
-	awk '{print $1,$2,$3}'    $dosefile > chr1.chunk1.dose
-	awk '{print $1,$2,$4}'    $dosefile > chr1.chunk2.dose
-	awk '{print $1,$2,$5,$6}' $dosefile > chr2.chunk1.dose
-	awk '{print $1,$2,$7}'    $dosefile > chr2.chunk2.dose
+        # ------------------ Chunks test ----------------------
+        # Split the dose and info files up into two chromosomes with
+        # some chunks
+        awk '{print $1,$2,$3}'    $dosefile > chr1.chunk1.dose
+        awk '{print $1,$2,$4}'    $dosefile > chr1.chunk2.dose
+        awk '{print $1,$2,$5,$6}' $dosefile > chr2.chunk1.dose
+        awk '{print $1,$2,$7}'    $dosefile > chr2.chunk2.dose
 
-	awk '{print $1,$2,$3,$4}'        $probfile > chr1.chunk1.prob
-	awk '{print $1,$2,$5,$6}'        $probfile > chr1.chunk2.prob
-	awk '{print $1,$2,$7,$8,$9,$10}' $probfile > chr2.chunk1.prob
-	awk '{print $1,$2,$11,$12}'      $probfile > chr2.chunk2.prob
+        awk '{print $1,$2,$3,$4}'        $probfile > chr1.chunk1.prob
+        awk '{print $1,$2,$5,$6}'        $probfile > chr1.chunk2.prob
+        awk '{print $1,$2,$7,$8,$9,$10}' $probfile > chr2.chunk1.prob
+        awk '{print $1,$2,$11,$12}'      $probfile > chr2.chunk2.prob
 
-	sed -n '1,2p' $infofile >  chr1.chunk1.info
-	sed -n '1p'   $infofile >  chr1.chunk2.info
-	sed -n '3p'   $infofile >> chr1.chunk2.info
-	sed -n '1p'   $infofile >  chr2.chunk1.info
-	sed -n '4,5p' $infofile >> chr2.chunk1.info
-	sed -n '1p'   $infofile >  chr2.chunk2.info
-	sed -n '6p'   $infofile >> chr2.chunk2.info
+        sed -n '1,2p' $infofile >  chr1.chunk1.info
+        sed -n '1p'   $infofile >  chr1.chunk2.info
+        sed -n '3p'   $infofile >> chr1.chunk2.info
+        sed -n '1p'   $infofile >  chr2.chunk1.info
+        sed -n '4,5p' $infofile >> chr2.chunk1.info
+        sed -n '1p'   $infofile >  chr2.chunk2.info
+        sed -n '6p'   $infofile >> chr2.chunk2.info
 
-	sed -n '1,2p' $mapfile >  chr1.chunk1.map
-	sed -n '1p'   $mapfile >  chr1.chunk2.map
-	sed -n '3p'   $mapfile >> chr1.chunk2.map
-	sed -n '1p'   $mapfile >  chr2.chunk1.map
-	sed -n '4,5p' $mapfile >> chr2.chunk1.map
-	sed -n '1p'   $mapfile >  chr2.chunk2.map
-	sed -n '6p'   $mapfile >> chr2.chunk2.map
+        sed -n '1,2p' $mapfile >  chr1.chunk1.map
+        sed -n '1p'   $mapfile >  chr1.chunk2.map
+        sed -n '3p'   $mapfile >> chr1.chunk2.map
+        sed -n '1p'   $mapfile >  chr2.chunk1.map
+        sed -n '4,5p' $mapfile >> chr2.chunk1.map
+        sed -n '1p'   $mapfile >  chr2.chunk2.map
+        sed -n '6p'   $mapfile >> chr2.chunk2.map
 
-	WithOrWithout="with"
+        WithOrWithout="with"
     else
-	echo "Run this function with one of these arguments: 'chunk'
-	or 'nochunk'."
-	exit 1
+        echo "Run this function with one of these arguments: 'chunk'
+        or 'nochunk'."
+        exit 1
     fi
 
 }
@@ -111,10 +111,10 @@ run_test ()
     ./probabel.pl 1 2 linear $1 --additive height
     echo -n "  Verifying $outfile: "
     if diff $outfile $results/$outfile; then
-	echo -e "\t\tOK"
+        echo -e "\t\tOK"
     else
-	echo -e "\t\tFAILED"
-	exit 1
+        echo -e "\t\tFAILED"
+        exit 1
     fi
 
     # Run an analysis on probabilities
@@ -123,19 +123,19 @@ run_test ()
     height_ngp2_add.out.txt"
 
     for file in $outfilelist; do
-	rm -f $file
+        rm -f $file
     done
 
     echo "Checking output using probabilities $WithOrWithout chunks..."
     ./probabel.pl 1 2 linear $1 --allmodels height -o _ngp2
     for file in $outfilelist; do
-	echo -n "  Verifying $file: "
-	if diff $file $results/$file; then
-	    echo -e "\t\tOK"
-	else
-	    echo -e "\t\tFAILED"
-	    exit 1
-	fi
+        echo -n "  Verifying $file: "
+        if diff $file $results/$file; then
+            echo -e "\t\tOK"
+        else
+            echo -e "\t\tFAILED"
+            exit 1
+        fi
     done
 }
 
