7167874e59b6103696a494f903fddb070d4330e8
Lennart C. Karssen <lennart@karssen.org>
2013-02-07 23:42:49 +0000

    - example_mms.sh:    * Added diff function to example    * also check the other models after running with probabilities    * minor rename of the output files when using dosage as input - Makefile.am:    * If --enable-pacoxph is set at ./configure time, also run the example (test) script    * Adapted to the new output filenames when using dosage as input
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/branches/ProbABEL-pacox/v.0.3.0/ProbABEL@1091 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- examples/example_mms.sh
+++ examples/example_mms.sh
@@ -1,4 +1,22 @@
-echo "analysis using MMSCORE"
+#!/bin/sh
+# This script runs checks on ProbABEL's palinear module with MMScore
+
+run_diff()
+{
+    # This function is run after each check. It needs two arguments:
+    # $1: first file to compare
+    # $2: second file to compare
+    if diff "$1" "$2"; then
+        echo -e "\t\tOK"
+    else
+        echo -e "\t\tFAILED"
+        exit 1
+    fi
+}
+
+
+# ---- The checks start here ----
+echo "Analysis using MMSCORE"
 if [ -z ${srcdir} ]; then
     srcdir="."
 fi
@@ -8,7 +26,7 @@ fi
     -i ${srcdir}/mmscore_gen.mlinfo \
     -d ${srcdir}/mmscore_gen.mldose \
     --sep="," \
-    -o mmscore \
+    -o mmscore_dose \
     --mmscore ${srcdir}/mmscore_InvSigma_aj.sex.age.dat
 
 ../src/palinear \
@@ -16,7 +34,7 @@ fi
     -i ${srcdir}/mmscore_gen.mlinfo \
     -d ${srcdir}/mmscore_gen.dose.fvi \
     --sep="," \
-    -o mmscore_fv \
+    -o mmscore_dose_fv \
     --mmscore ${srcdir}/mmscore_InvSigma_aj.sex.age.dat
 
 echo "mmscore check: dose vs. dose_fv"
@@ -39,8 +57,16 @@ diff mmscore_add.out.txt mmscore_fv_add.out.txt
     -o mmscore_prob_fv \
     --mmscore ${srcdir}/mmscore_InvSigma_aj.sex.age.dat
 
-echo "mmscore check: prob vs. prob_fv"
-diff mmscore_prob_add.out.txt mmscore_prob_fv_add.out.txt
+echo -n "mmscore check (additive model): prob vs. prob_fv "
+run_diff mmscore_prob_add.out.txt mmscore_prob_fv_add.out.txt
+echo -n "mmscore check (dominant model): prob vs. prob_fv "
+run_diff mmscore_prob_domin.out.txt mmscore_prob_fv_domin.out.txt
+echo -n "mmscore check (recessive model): prob vs. prob_fv "
+run_diff mmscore_prob_recess.out.txt mmscore_prob_fv_recess.out.txt
+echo -n "mmscore check (over-dominant model): prob vs. prob_fv "
+run_diff mmscore_prob_over_domin.out.txt mmscore_prob_fv_over_domin.out.txt
+echo -n "mmscore check (2df model): prob vs. prob_fv "
+run_diff mmscore_prob_2df.out.txt mmscore_prob_fv_2df.out.txt
 
 # Commented out because of slightly different output formats. We need
 # something smart here.
