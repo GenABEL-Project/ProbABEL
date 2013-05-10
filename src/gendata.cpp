1e680c26d1ea2683a9c829bf9583d6b31e3fed88
Lennart C. Karssen <lennart@karssen.org>
2013-05-10 21:38:05 +0000

    In ProbABEL's gendata.cpp: Cleaner implementation of the conversion from string to double when reading the genetic data (both from text files and .fv? files. Based on the strtod and strtol man pages. Now Vlagrind doesn't complain about jumps based on uninitialised values anymore. Furthermore, the invalied read sizes of 8 bytes have also gone.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/ProbABEL@1211 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/gendata.cpp
+++ src/gendata.cpp
@@ -5,6 +5,7 @@
  *      Author: mkooyman
  */
 #include <string>
+#include <errno.h>
 #include "gendata.h"
 #include "fvlib/FileVector.h"
 #if EIGEN
@@ -41,7 +42,26 @@ void gendata::get_var(int var, double * data)
                 std::ostringstream strs;
                 strs << tmpdata[i];
                 std::string str = strs.str();
-                data[j++] = strtod(str.c_str(), (char **) NULL);
+                double val;
+                char *endptr;
+                errno = 0;      // To distinguish success/failure
+                                // after strtod()
+                val = strtod(str.c_str(), &endptr);
+
+                if ((errno == ERANGE && (val == HUGE_VALF || val == HUGE_VALL))
+                    || (errno != 0 && val == 0)) {
+                    perror("Error while reading genetic data (strtod)");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (endptr == str.c_str()) {
+                    cerr << "No digits were found while reading genetic data"
+                         << endl;
+                    exit(EXIT_FAILURE);
+                }
+
+                /* If we got here, strtod() successfully parsed a number */
+                data[j++] = val;
             }
         }
     }
@@ -171,9 +191,26 @@ void gendata::re_gendata(char * fname, unsigned int insnps,
                     infile >> tmpstr;
                     // tmpstr contains the dosage in string form. Convert
                     // it to double (if tmpstr is NaN it will be set to nan).
-                    // Note that Valgrind 3.7.0 gives "Invalid read of
-                    // size 8" error messages here. A bug in Valgrind?!
-                    double dosage = strtod(tmpstr.c_str(), (char **) NULL);
+                    double dosage;
+                    char *endptr;
+                    errno = 0;      // To distinguish success/failure
+                                    // after strtod()
+                    dosage = strtod(tmpstr.c_str(), &endptr);
+
+                    if ((errno == ERANGE &&
+                         (dosage == HUGE_VALF || dosage == HUGE_VALL))
+                        || (errno != 0 && dosage == 0)) {
+                        perror("Error while reading genetic data (strtod)");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    if (endptr == tmpstr.c_str()) {
+                        cerr << "No digits were found while reading genetic data"
+                             << endl;
+                        exit(EXIT_FAILURE);
+                    }
+
+                    /* If we got here, strtod() successfully parsed a number */
                     G.put(dosage, k, j);
                 }
                 else
