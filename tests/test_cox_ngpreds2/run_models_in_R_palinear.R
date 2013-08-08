7b23389cbac125899f10bdbf84215f029620e1aa
Lennart C. Karssen <lennart@karssen.org>
2013-08-08 11:23:24 +0000

    Updated the ProbABEL tests to reflect the changes in the previous commit where chi^2 values were added to the output of palinear.
    Extended tests/test_cox_ngpreds2/run_models_in_R_palinear.R (actually
    that directory should be renamed now...) so that the ProbABEL output
    is now compared to the R output and errors are printed when there is a
    descrepancy. I've set the tolerance for the checks to 1e-5.
    
    Copyright of this change lies with the Erasmus MC.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/ProbABEL@1287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/test_cox_ngpreds2/run_models_in_R_palinear.R
+++ tests/test_cox_ngpreds2/run_models_in_R_palinear.R
@@ -1,74 +1,241 @@
-###
-# load the data
-###
+## Set tolerance for comparing various outputs
+tol <- 1e-5
 
-# load phenotypic data
+####
+## load the data
+####
+
+## load phenotypic data
 pheno <- read.table("../../examples/height.txt", head=TRUE, string=FALSE)
 
-# load genetic DOSE data
+## load genetic DOSE data
 dose <- read.table("../../examples/test.mldose", head=FALSE, string=FALSE)
-# remove "1->" from the names of dose-IDs
+## remove "1->" from the names of dose-IDs
 idNames <- dose[, 1]
 idNames <- sub("[0-9]+->", "", idNames)
 dose[, 1] <- idNames
-# check consistency of names
-table(dose[, 1] == pheno[, 1])
+cat("Dose: check consistency of names\t\t")
+stopifnot( all.equal(dose[, 1], pheno[, 1], tol) )
+cat("OK\n")
 
-# load genetic PROB data
+## load genetic PROB data
 prob <- read.table("../../examples/test.mlprob", head=FALSE, string=FALSE)
-# remove "1->" from the names of prob-IDs
+## remove "1->" from the names of prob-IDs
 idNames <- prob[, 1]
 idNames <- sub("[0-9]+->", "", idNames)
 prob[, 1] <- idNames
-# check consistency of names
-table(prob[, 1] == pheno[, 1])
+cat("Prob: check consistency of names\t\t")
+stopifnot( all.equal(prob[, 1], pheno[, 1], tol) )
+cat("OK\n")
 
-# check consistency DOSE <-> PROB
+## check consistency DOSE <-> PROB
 doseFromProb <- matrix(NA, ncol=dim(dose)[2], nrow=dim(dose)[1])
 for (i in 3:dim(dose)[2]) {
         indexHom <- 3 + ( i - 3 ) * 2
         indexHet <- indexHom + 1
         doseFromProb[, i] <- prob[, indexHom] * 2 + prob[, indexHet]
-        print( table( ( dose[, i] - doseFromProb[, i] ) > 1e-8 ) )
 }
+cat("Check consistency dose <-> prob\t\t\t")
+stopifnot( all.equal(dose[, 3:ncol(dose)],
+                     as.data.frame(doseFromProb)[,3:ncol(doseFromProb)],
+                     tol=tol )
+          )
+cat("OK\n")
 
-###
-# run analysis
-###
+####
+## Run ProbABEL to get the output data we want to compare/verify
+####
+cat("Running ProbABEL...\t\t\t\t")
+tmp <- system("cd ../../examples/; sh example_qt.sh; cd -",
+              intern=TRUE)
+cat("OK\n")
 
+resPaAddDose <-
+    read.table("../../examples/height_base_add.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_add",
+                     "sebeta_SNP_add",
+                     "chi2_SNP")]
+resPaAddProb <-
+    read.table("../../examples/height_ngp2_add.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_addA1",
+                     "sebeta_SNP_addA1",
+                     "chi2_SNP_A1")]
+resPaDom <-
+    read.table("../../examples/height_ngp2_domin.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_domA1",
+                     "sebeta_SNP_domA1",
+                     "chi2_SNP_domA1")]
+resPaRec <-
+    read.table("../../examples/height_ngp2_recess.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_recA1",
+                     "sebeta_SNP_recA1",
+                     "chi2_SNP_recA1")]
+resPaOdom <-
+    read.table("../../examples/height_ngp2_over_domin.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_odomA1",
+                     "sebeta_SNP_odomA1",
+                     "chi2_SNP_odomA1")]
+resPa2df <-
+    read.table("../../examples/height_ngp2_2df.out.txt",
+               head=TRUE)[,
+                   c("beta_SNP_A1A2",
+                     "sebeta_SNP_A1A2",
+                     "beta_SNP_A1A1",
+                     "sebeta_SNP_A1A1",
+                     "chi2_SNP_2df")]
+
+####
+## run analysis in R
+####
 attach(pheno)
 
-# run analysis on dose
+cat("Comparing R output with ProbABEL output\t\t")
+## run analysis on dose
+dose.add.R <- data.frame(beta_SNP_add   = numeric(),
+                         sebeta_SNP_add = numeric(),
+                         chi2_SNP       = numeric())
+prob.add.R <- data.frame(beta_SNP_addA1   = numeric(),
+                         sebeta_SNP_addA1 = numeric(),
+                         chi2_SNP_A1      = numeric())
 for (i in 3:dim(dose)[2]) {
-        smr <- summary( lm( height ~ sex + age + dose[, i] ) )
-        print( smr$coef[4, 1:2] )
-        smr <- summary( lm( height ~ sex + age + doseFromProb[, i] ) )
-        print( smr$coef[4, 1:2] )
-}
+        noNA <- which( !is.na(dose[, i]) )
+        model.0 <- lm( height[noNA] ~ sex[noNA] + age[noNA] )
 
-# dominant model
+        model.dose <- lm( height ~ sex + age + dose[, i] )
+        sm.dose <- summary(model.dose)$coef[4, 1:2]
+
+        model.prob <- lm( height ~ sex + age + doseFromProb[, i] )
+        sm.prob <- summary(model.prob)$coef[4, 1:2]
+
+        lrt.dose <- 2 * ( logLik( model.dose ) - logLik( model.0 ) )
+        lrt.prob <- 2 * ( logLik( model.prob ) - logLik( model.0 ) )
+
+        row <- data.frame(
+            beta_SNP_add = sm.dose[1],
+            sebeta_SNP_add = sm.dose[2],
+            chi2_SNP = lrt.dose)
+        dose.add.R <- rbind(dose.add.R, row)
+
+        row <- data.frame(
+            beta_SNP_addA1 = sm.prob[1],
+            sebeta_SNP_addA1 = sm.prob[2],
+            chi2_SNP_A1 = lrt.prob)
+        prob.add.R <- rbind(prob.add.R, row)
+}
+rownames(dose.add.R) <- NULL
+rownames(prob.add.R) <- NULL
+stopifnot( all.equal(resPaAddDose, dose.add.R, tol=tol) )
+
+stopifnot( all.equal(resPaAddProb, prob.add.R, tol=tol) )
+
+
+## dominant model
+prob.dom.R <- data.frame(beta_SNP_domA1   = numeric(),
+                         sebeta_SNP_domA1 = numeric(),
+                         chi2_SNP_domA1   = numeric())
 for (i in 3:dim(dose)[2]) {
         indexHom <- 3 + ( i - 3 ) * 2
         indexHet <- indexHom + 1
-        regProb <- prob[, indexHom] + prob[, indexHet]
-        smr <- summary( lm( height ~ sex + age + regProb ) )
-        print( smr$coef[4, 1:2] )
-}
+        regProb  <- prob[, indexHom] + prob[, indexHet]
 
-# recessive model
+        noNA    <- which( !is.na(regProb) )
+        model.0 <- lm( height[noNA] ~ sex[noNA] + age[noNA] )
+        model   <- lm( height ~ sex + age + regProb )
+        sm      <- summary(model)$coef[4, 1:2]
+        lrt     <- 2 * ( logLik( model ) - logLik( model.0 ) )
+
+        row <- data.frame(
+            beta_SNP_domA1   = sm[1],
+            sebeta_SNP_domA1 = sm[2],
+            chi2_SNP_domA1   = lrt)
+        prob.dom.R <- rbind(prob.dom.R, row)
+}
+rownames(prob.dom.R) <- NULL
+stopifnot( all.equal(resPaDom, prob.dom.R, tol=tol) )
+
+
+## recessive model
+prob.rec.R <- data.frame(beta_SNP_recA1   = numeric(),
+                         sebeta_SNP_recA1 = numeric(),
+                         chi2_SNP_recA1   = numeric())
 for (i in 3:dim(dose)[2]) {
         indexHom <- 3 + ( i - 3 ) * 2
         indexHet <- indexHom + 1
         regProb <- prob[, indexHom]
-        smr <- summary( lm( height ~ sex + age + regProb ) )
-        print( smr$coef[4, 1:2] )
-}
 
-# over-dominant model
+        noNA    <- which( !is.na(regProb) )
+        model.0 <- lm( height[noNA] ~ sex[noNA] + age[noNA] )
+        model   <- lm( height ~ sex + age + regProb )
+        sm      <- summary(model)$coef[4, 1:2]
+        lrt     <- 2 * ( logLik( model ) - logLik( model.0 ) )
+
+        row <- data.frame(
+            beta_SNP_recA1   = sm[1],
+            sebeta_SNP_recA1 = sm[2],
+            chi2_SNP_recA1   = lrt)
+        prob.rec.R <- rbind(prob.rec.R, row)
+}
+rownames(prob.rec.R) <- NULL
+stopifnot( all.equal(resPaRec, prob.rec.R, tol=tol) )
+
+
+## over-dominant model
+prob.odom.R <- data.frame(beta_SNP_odomA1   = numeric(),
+                         sebeta_SNP_odomA1 = numeric(),
+                         chi2_SNP_odomA1   = numeric())
 for (i in 3:dim(dose)[2]) {
         indexHom <- 3 + ( i - 3 ) * 2
         indexHet <- indexHom + 1
         regProb <- prob[, indexHet]
-        smr <- summary( lm( height ~ sex + age + regProb ) )
-        print( smr$coef[4, 1:2] )
+
+        noNA    <- which( !is.na(regProb) )
+        model.0 <- lm( height[noNA] ~ sex[noNA] + age[noNA] )
+        model   <- lm( height ~ sex + age + regProb )
+        sm      <- summary(model)$coef[4, 1:2]
+        lrt     <- 2 * ( logLik( model ) - logLik( model.0 ) )
+
+        row <- data.frame(
+            beta_SNP_odomA1   = sm[1],
+            sebeta_SNP_odomA1 = sm[2],
+            chi2_SNP_odomA1   = lrt)
+        prob.odom.R <- rbind(prob.odom.R, row)
 }
+rownames(prob.odom.R) <- NULL
+stopifnot( all.equal(resPaOdom, prob.odom.R, tol=tol) )
+
+
+## 2df model
+prob.2df.R <- data.frame(beta_SNP_A1A2   = numeric(),
+                          sebeta_SNP_A1A2 = numeric(),
+                          beta_SNP_A1A1   = numeric(),
+                          sebeta_SNP_A1A1 = numeric(),
+                         chi2_SNP_2df     = numeric())
+for (i in 3:dim(dose)[2]) {
+        indexHom <- 3 + ( i - 3 ) * 2
+        indexHet <- indexHom + 1
+        regProb <- prob[, indexHet]
+
+        noNA    <- which( !is.na(regProb) )
+        model.0 <- lm( height[noNA] ~ sex[noNA] + age[noNA] )
+        model   <- lm( height ~ sex + age + prob[, indexHet] + prob[, indexHom] )
+        smA1A2  <- summary(model)$coef[4, 1:2]
+        smA1A1  <- summary(model)$coef[5, 1:2]
+        lrt     <- 2 * ( logLik( model ) - logLik( model.0 ) )
+
+        row <- data.frame(
+            beta_SNP_A1A2   = smA1A2[1],
+            sebeta_SNP_A1A2 = smA1A2[2],
+            beta_SNP_A1A1   = smA1A1[1],
+            sebeta_SNP_A1A1 = smA1A1[2],
+            chi2_SNP_2df   = lrt)
+        prob.2df.R <- rbind(prob.2df.R, row)
+}
+rownames(prob.2df.R) <- NULL
+stopifnot( all.equal(resPa2df, prob.2df.R, tol=tol) )
+
+cat("OK\n")
